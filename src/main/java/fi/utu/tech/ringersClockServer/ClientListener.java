489008f5bc627a274e7f773d7235d909706136be
olli.porvari <olli.porvari@eduvantaa.fi>
2021-01-27 13:12:30 +0200

    Wakeupservicea muokattu.

--- src/main/java/fi/utu/tech/ringersClockServer/ClientListener.java
+++ src/main/java/fi/utu/tech/ringersClockServer/ClientListener.java
@@ -12,7 +12,6 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.io.IOException;
 
-
 public class ClientListener extends Thread {
 
 	private Socket client;
@@ -30,30 +29,31 @@ public class ClientListener extends Thread {
 			OutputStream oS = client.getOutputStream();
 			ObjectOutputStream oOut = new ObjectOutputStream(oS);
 			ObjectInputStream oIn = new ObjectInputStream(iS);
-			try {
-				Object obj = oIn.readObject();
-				if (obj instanceof AlarmConfirm) {
-				  wup.handleAlarmConfirm((AlarmConfirm)obj);
-				}
-				if (obj instanceof JoinMessage) {
-					wup.handleJoin(this, (JoinMessage)obj);
-				}
-				if (obj instanceof ResignMessage) {
-					wup.handleResign(this);
-				}
-				
-			} catch (IOException e) {
-				oIn.close();
-		} catch (Exception e) {
-			throw new Error(e.toString());
+
+			Object obj = oIn.readObject();
+			if (obj instanceof AlarmConfirm) {
+				wup.handleAlarmConfirm((AlarmConfirm) obj);
 			}
+			if (obj instanceof JoinMessage) {
+				wup.handleJoin(this, (JoinMessage) obj);
+			}
+			if (obj instanceof ResignMessage) {
+				wup.handleResign(this);
+			}
+			oIn.close();
+		} catch (IOException e) {
+			throw new Error(e.toString());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-		
+
 	public static void send(Serializable s) {
 		try {
 			oOut.writeObject(s);
 			oOut.flush();
+			oOut.close();
 		} catch (IOException e) {
 			try {
 				oOut.close();
@@ -62,7 +62,7 @@ public class ClientListener extends Thread {
 				e1.printStackTrace();
 			}
 		} catch (Exception e) {
-				throw new Error(e.toString());
+			throw new Error(e.toString());
 		}
 	}
 }
--- src/main/java/fi/utu/tech/ringersClockServer/WakeUpService.java
+++ src/main/java/fi/utu/tech/ringersClockServer/WakeUpService.java
@@ -1,15 +1,34 @@
 package fi.utu.tech.ringersClockServer;
 
+import java.time.LocalTime;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+
 import fi.utu.tech.ringersClock.entities.AlarmConfirm;
 import fi.utu.tech.ringersClock.entities.JoinMessage;
+import fi.utu.tech.ringersClock.entities.WakeUpGroup;
 
 public class WakeUpService extends Thread {
 
+	private Integer UUID;
+	private WakeUpGroup wakeUpGroup;
+	private Vector<WakeUpGroup> groups;
+	private Vector<WakeUpGroup> groups2;
+	private ConcurrentHashMap<Integer, Vector<WakeUpGroup>> groupsWithID = new ConcurrentHashMap<>();
+	private ConcurrentHashMap<LocalTime, Vector<WakeUpGroup>> wakeuptimes;
+	private Integer timeHour;
+	private Integer timeMinutes;
+	private LocalTime wakeUpTime;
+	private LocalTime timeNow;
+	
 	public WakeUpService() {
 
 	}
 
 	public void run() {
+		//herätysajan vertailu oikeaan aikaan minuutin välein
+		
+		//kun herätys, looppaisi groups2:n johtajat läpi ja lähettäisi herätysviestin
 
 	}
 
@@ -18,6 +37,21 @@ public class WakeUpService extends Thread {
 			ClientListener.send(obj);
 		}
 		
+	}
+
+public void setGroupIdList(WakeUpGroup wakeUpGroup) {
+		UUID = wakeUpGroup.getID();
+		groups.add(wakeUpGroup);
+		groupsWithID.put(UUID, groups);
+		setGroupTimeList(wakeUpGroup);
+	}
+	
+	public void setGroupTimeList(WakeUpGroup wakeUpGroup) {
+		timeHour = wakeUpGroup.getHour();
+		timeMinutes = wakeUpGroup.getMinutes();
+		wakeUpTime = LocalTime.of(timeHour, timeMinutes);
+		groups2.add(wakeUpGroup);
+		wakeuptimes.put(wakeUpTime, groups2);
 	}
 
 	public void handleJoin(ClientListener client, JoinMessage message) {
